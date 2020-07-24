79fea4452856aef6b5cdd15240eba3d8b9eab05f
melihcanclk <melihcanclk@hotmail.com>
2020-07-24 23:01:20 +0300

    Move twitch channels to a files.js file Add node_modules to gitignore

---
+++ files.js
@@ -0,0 +1,3 @@
+var channels = ['russz00','ubeka_', 'purplebixi','Legoo',  'wtcn','napofsnorlax', 'melihcancilek','cned','yamito', 'thaldrinlol']
+
+module.exports = channels
--- oauth.js
+++ oauth.js
@@ -1,9 +1,10 @@
-const { channels } = require("./options")
+
+var channels_out = require('./files')
 
 var infos = {
     oauth : "oauth:owqehqi9byc3t2pvvrwpk6a43osmhk",
     username : "melihcancilek",
-    channels : ['russz00','ubeka_', 'purplebixi','Legoo',  'wtcn','napofsnorlax', 'melihcancilek','cned','yamito', 'thaldrinlol']
+    channels : channels_out
 }
 
 module.exports = infos
